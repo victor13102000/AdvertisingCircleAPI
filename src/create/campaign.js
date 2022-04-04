f4b4dba15ab1af73c6d30b60d179ec7980a807a4
matiasrocca <matiasrocca98@gmail.com>
2022-04-04 19:11:01 -0300

    nuevas rutas para las campañas

--- src/create/campaign.js
+++ src/create/campaign.js
@@ -55,13 +55,10 @@ async function run(req, res, databaseConnection) {
 
             };
 
-            const usersCollection = databaseConnection.db("adpolygon").collection("users");
             const campaignsCollection = databaseConnection.db('adpolygon').collection('campaigns');
-
-            const previousAdvertiser = await usersCollection.findOne({ username: advertiser });
             const previousCampaign = await campaignsCollection.findOne({ name: body.name });
 
-            if(previousAdvertiser && previousCampaign){
+            if(previousCampaign){
                 return res.status(400).json({
                     'success': false,
                     'message': 'The user has already created a campaing with this name.'
@@ -100,10 +97,126 @@ async function update(req, res, databaseConnection) {
 
 }
 
+async function advertiserCampaigns(req, res, databaseConnection) {
+
+    try {
+        const token = req.body.token;
+    
+        const config = {
+          headers: { Authorization: `Bearer ${token}` },
+        };
+        const bodyParameters = {
+          "Content-Type": "application/json",
+        };
+        const prueba = await axios.post(
+          "https://accounts.clusterby.com/auth",
+          bodyParameters,
+          config
+        );
+    
+        const advertiser = prueba.data.username;
+
+        const campaignsCollection = databaseConnection.db('adpolygon').collection('campaigns');
+
+        const allCampaigns = await campaignsCollection.findOne({ advertiser:advertiser });
+
+        if(allCampaigns) {
+            return res.status(200).json({
+                success: true,
+                message: "Campaigns Ok.",
+                campaigns: allCampaigns,
+              });
+        }else {
+            return res.status(400).json({
+                success: true,
+                message: "Advertiser hasn´t uploaded any campaign yet."
+              });
+        }
+    } catch (error) {
+        console.log( error);
+      }
+
+}
+
+async function advertiserSpecificCampaign(req, res, databaseConnection) {
+    var ObjectId = require('mongodb').ObjectId; 
+
+    try {
+        const token = req.body.token;
+    
+        const config = {
+          headers: { Authorization: `Bearer ${token}` },
+        };
+        const bodyParameters = {
+          "Content-Type": "application/json",
+        };
+        const prueba = await axios.post(
+          "https://accounts.clusterby.com/auth",
+          bodyParameters,
+          config
+        );
+    
+        const advertiser = prueba.data.username;
+        const _id = req.body.id
+
+        const campaignsCollection = databaseConnection.db('adpolygon').collection('campaigns');
+
+        const campaign = await campaignsCollection.findOne({_id: ObjectId(_id)})
+
+        if(campaign && (campaign.advertiser === advertiser)) {
+            return res.status(200).json({
+                success: true,
+                message: "Campaigns Ok.",
+                campaigns: campaign,
+              });
+        }else {
+            return res.status(400).json({
+                success: true,
+                message: "Something wen´t wrong, information not available."
+              });
+        }
+    } catch (error) {
+        console.log( error);
+      }
+
+}
+
+async function allCampaigns (req, res, databaseConnection) {
+
+    try {
+        const campaignsCollection = databaseConnection.db('adpolygon').collection('campaigns');
+
+        const allCampaigns = await campaignsCollection.find();
+
+        console.log(allCampaigns)
+
+        if(allCampaigns) {
+            return res.status(200).json({
+                success: true,
+                message: "Campaigns Ok.",
+                campaigns: allCampaigns,
+              });
+        }else {
+            return res.status(400).json({
+                success: true,
+                message: "No campaign has been uploaded yet."
+              });
+        }
+
+
+    } catch (error) {
+        console.log( error);
+      }
+
+}
+
 
 
 module.exports = {
 
-    "run": run
+    "run": run,
+    "allCampaigns": allCampaigns,
+    "advertiserCampaigns": advertiserCampaigns,
+    "advertiserSpecificCampaign": advertiserSpecificCampaign
 
 }
