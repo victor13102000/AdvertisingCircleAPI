6bdec76dd74295259d3060a10edabf0d8e9a1d4f
VirClancy <virginiaclancy@gmail.com>
2022-04-05 17:20:51 -0300

    ruta modificacion de campaña

--- src/Routes/campaign.js
+++ src/Routes/campaign.js
@@ -1,17 +1,20 @@
-const express = require("express"); 
-const campaign = express.Router() 
+const express = require("express");
+const campaign = express.Router();
 const createCampaign = require("../create/campaign");
 const database = require("../database.js");
 global.config = require("../../config.json");
 
-
 (async () => {
- const databaseConnection = await database.getConnection(
-   global.config.database.url
- );
+  const databaseConnection = await database.getConnection(
+    global.config.database.url
+  );
 
-campaign.post("/create", (req, res) => {
+  campaign.post("/create", (req, res) => {
     createCampaign.run(req, res, databaseConnection);
   });
+
+  campaign.put("/update", (req, res) => {
+    createCampaign.updateCampaigns(req, res, databaseConnection);
+  });
 })();
-module.exports= campaign
+module.exports = campaign;
--- src/create/campaign.js
+++ src/create/campaign.js
@@ -7,103 +7,182 @@ axios.default.httpsAgent = new https.Agent({
 
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 
-
 async function run(req, res, databaseConnection) {
+  try {
+    const body = req.body;
 
-    try {
-        const body = req.body;
-    
-        const token = req.body.token;
-    
-        const config = {
-          headers: { Authorization: `Bearer ${token}` },
-        };
-        const bodyParameters = {
-          "Content-Type": "application/json",
-        };
-        const prueba = await axios.post(
-          "https://accounts.clusterby.com/auth",
-          bodyParameters,
-          config
-        );
+    const token = req.body.token;
 
-        const advertiser = prueba.data.username;
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const bodyParameters = {
+      "Content-Type": "application/json",
+    };
+    const prueba = await axios.post(
+      "https://accounts.clusterby.com/auth",
+      bodyParameters,
+      config
+    );
 
-        const name = body.name;
-        const description = body.description;
-        const startDate = body.startDate;
-        const endDate = body.endDate;
-        const type = body.type;
-        const data = body.data;
-        const imgUrl = body.img;
+    const advertiser = prueba.data.username;
 
-        // URL campaign
-        if (type == 0) {
+    const name = body.name;
+    const description = body.description;
+    const startDate = body.startDate;
+    const endDate = body.endDate;
+    const type = body.type;
+    const data = body.data;
+    const imgUrl = body.img;
 
-            const state = 1; // init state
+    // URL campaign
+    if (type == 0) {
+      const state = 1; // init state
 
-            const campaign = {
+      const campaign = {
+        name: name,
+        imgUrl: imgUrl,
+        description: description,
+        state: state,
+        advertiser: advertiser,
+        startDate: startDate,
+        endDate: endDate,
+        data: data,
+      };
 
-                'name': name,
-                "imgUrl": imgUrl,
-                'description': description,
-                'state': state,
-                'advertiser': advertiser,
-                'startDate': startDate,
-                'endDate': endDate,
-                'data': data,
+      const campaignsCollection = databaseConnection
+        .db("adpolygon")
+        .collection("campaigns");
+      const previousCampaign = await campaignsCollection.findOne({
+        name: body.name,
+      });
 
-            };
+      if (previousCampaign) {
+        return res.status(400).json({
+          success: false,
+          message: "The user has already created a campaing with this name.",
+        });
+      }
 
-            const usersCollection = databaseConnection.db("adpolygon").collection("users");
-            const campaignsCollection = databaseConnection.db('adpolygon').collection('campaigns');
+      if (new Date(`<${startDate}>`) < new Date()) {
+        return res.status(400).json({
+          success: false,
+          message: "Start date has already passed.",
+        });
+      }
 
-            const previousAdvertiser = await usersCollection.findOne({ username: advertiser });
-            const previousCampaign = await campaignsCollection.findOne({ name: body.name });
+      if (new Date(`<${startDate}>`) > new Date(`<${endDate}>`)) {
+        return res.status(400).json({
+          success: false,
+          message: "End date cannot be before Start date.",
+        });
+      }
 
-            if(previousAdvertiser && previousCampaign){
-                return res.status(400).json({
-                    'success': false,
-                    'message': 'The user has already created a campaing with this name.'
-                })
-            }
+      const resCampaign = await campaignsCollection.insertOne(campaign);
 
-            if(new Date(`<${startDate}>`)< new Date()){
-                return res.status(400).json({
-                    'success': false,
-                    'message': 'Start date has already passed.'
-                })
-            }
-
-            if(new Date(`<${startDate}>`) > new Date(`<${endDate}>`)){
-                return res.status(400).json({
-                    'success': false,
-                    'message': 'End date cannot be before Start date.'
-                })
-            }
-
-            await campaignsCollection.insertOne(campaign);
-
-            res.status(200).json({
-
-                'success': true,
-                'message': 'The campaign has been succesfully created.'
-
-            });
-        }
-    } catch (err) {
+      res.status(200).json({
+        success: true,
+        message: "The campaign has been succesfully created.",
+        ressultado: resCampaign,
+      });
+    }
+  } catch (err) {
     console.log(err);
   }
 }
 
-async function update(req, res, databaseConnection) {
+async function updateCampaigns(req, res, databaseConnection) {
+  var ObjectId = require("mongodb").ObjectId;
+  try {
+    const token = req.body.token;
 
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    const bodyParameters = {
+      "Content-Type": "application/json",
+    };
+    const prueba = await axios.post(
+      "https://accounts.clusterby.com/auth",
+      bodyParameters,
+      config
+    );
+
+    const user = prueba.data.username;
+
+    const usersCollection = databaseConnection
+      .db("adpolygon")
+      .collection("users");
+    const userInfo = await usersCollection.findOne({ username: user });
+
+    const userType = userInfo.type;
+
+    if (userType === "publisher") {
+      return res.status(400).json({
+        message: "User is not an advertiser",
+        success: false,
+      });
+    }
+    const _id = ObjectId(req.body.id);
+
+    const {
+      advertiser,
+      name,
+      description,
+      startDate,
+      endDate,
+      state,
+      type,
+      data,
+      imgUrl,
+    } = req.body;
+
+    const campaignsCollection = databaseConnection
+      .db("adpolygon")
+      .collection("campaigns");
+
+    const filter = {
+      _id: _id,
+    };
+
+    const options = { upset: false };
+
+    const updateDoc = {
+      $set: {
+        name: name != "" && name,
+        imgUrl: imgUrl != "" && imgUrl,
+        description: description != "" && description,
+        startDate: startDate != "" && startDate,
+        endDate: endDate != "" && endDate,
+        data: data != "" && data,
+      },
+    };
+
+    if (state === 1 && user === advertiser) {
+      const result = await campaignsCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(
+        `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+      );
+      res.status(200).json({
+        message: "update data",
+        success: true,
+      });
+    } else {
+      res.status(400).json({
+        message: "update not available",
+        success: false,
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-
 
 module.exports = {
-
-    "run": run
-
-}
+  run: run,
+  updateCampaigns: updateCampaigns,
+};
